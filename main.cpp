2db4dc41959891d4dfed4762d663665154bb0519
zjw1918 <zjw1918@sina.cn>
2023-05-09 18:40:56 +0800

    lambda

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <functional>
 
 void HelloWorld(int a)
 {
@@ -8,7 +9,8 @@ void HelloWorld(int a)
 }
 
 
-void ForEach(const std::vector<int>& values, void(*func)(int))
+// void ForEach(const std::vector<int>& values, void(*func)(int))
+void ForEach(const std::vector<int>& values, const std::function<void(int)>& func)
 {
     for (int value: values) {
         func(value);
@@ -29,9 +31,19 @@ int main()
 
 
     std::vector<int> arr = {0,1,2,3,4,10};
+
+    // an example to use lambda in fine_if
+    auto it = std::find_if(arr.begin(), arr.end(), [](int value) { return value > 3; });
+    std::cout << *it << std::endl;
+    std::cout << sizeof(it) / sizeof(int) << std::endl;
+
     ForEach(arr, fn);
     // lambda
-    ForEach(arr, [](int value) { std::cout << "Lambda: " << value << std::endl; });
+    int a = 1;
+    // =, &, a, &a
+    // all var by value, all var by refs, ..., ...
+    ForEach(arr, [&a](int value) mutable { a++; std::cout << "Lambda: " << a << std::endl; });
+    std::cout << "at last a: " << a << std::endl;
 
     return 0;
 }
